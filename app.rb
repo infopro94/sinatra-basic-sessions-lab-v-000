629e6f567d7a4ea429a01ed88759ef604ca068e1
Susan Fowler <infopro94@gmail.com>
2018-01-07 01:33:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,4 +11,11 @@ class App < Sinatra::Base
     erb :index
   end
 
+  get '/checkout' do
+    #session hash << params from form 
+    #key = item & value = item in user's cart
+    #instance variable = {session hash} - access in views
+    #display cart item in view
+  end
+
 end
